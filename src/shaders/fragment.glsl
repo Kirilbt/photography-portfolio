3f91a364b44698cd5198407c9dad1892ecc6ef2c
Kiril Tucker <kiril.tucker@gmail.com>
2022-08-23 18:41:43 +0200

    Combining Shaders

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -3,6 +3,7 @@ uniform sampler2D uTexture;
 
 varying float pulse;
 varying vec2 vUv;
+varying vec3 vNormal;
 
 void main() {
   vec4 myImage = texture(
@@ -11,4 +12,5 @@ void main() {
   );
   float sinePulse = (1. + sin(vUv.x*50. + time))*0.5;
   gl_FragColor = myImage;
+  gl_FragColor = vec4(0.5*(pulse+1.),0.,0.,1.);
 }
