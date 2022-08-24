3a1c04d9cd2a862d99bb6a414ed53e12de0d1baf
Kiril Tucker <kiril.tucker@gmail.com>
2022-08-24 09:44:48 +0200

    Created Zoom Effect

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import GUI from 'lil-gui'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
-import testTexture from './textures/water.jpg'
+import testTexture from './textures/test.jpg'
 
 export default class Sketch {
   constructor(options) {
@@ -26,10 +27,21 @@ export default class Sketch {
 
     this.time = 0
 
+
+    this.setupSettings()
     this.resize()
     this.addObjects()
     this.render()
     this.setupResize()
+  }
+
+  setupSettings() {
+    this.settings = {
+      progress: 0
+    }
+
+    this.gui = new GUI()
+    this.gui.add( this.settings, 'progress', 0, 1, 0.001)
   }
 
   resize() {
@@ -45,14 +57,16 @@ export default class Sketch {
   }
 
   addObjects() {
-    this.geometry = new THREE.PlaneGeometry( 350, 350, 100, 100 )
+    this.geometry = new THREE.PlaneGeometry( 300, 300, 100, 100 )
 
     this.material = new THREE.ShaderMaterial({
       wireframe: false,
       uniforms: {
-        time: { value: 1.0 },
-        uTexture: { value: new THREE.TextureLoader().load(testTexture)},
-        resolution: { value: new THREE.Vector2() }
+        time: {value: 1.0},
+        uProgress: {value: 1.0},
+        uTexture: {value: new THREE.TextureLoader().load(testTexture)},
+        uResolution: {value: new THREE.Vector2(this.width, this.height)},
+        uQuadSize: {value: new THREE.Vector2(300, 300)}
       },
 
       vertexShader: vertex,
@@ -61,11 +75,15 @@ export default class Sketch {
 
     this.cube = new THREE.Mesh( this.geometry, this.material )
     this.scene.add( this.cube )
+    this.cube.position.x = 300
+    this.cube.rotation.x = 0.5
   }
 
   render() {
     this.time += 0.05
     this.material.uniforms.time.value = this.time
+    this.material.uniforms.uProgress.value = this.settings.progress
+
     // this.cube.rotation.x += 0.01
     // this.cube.rotation.y += 0.01
 
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,5 +1,19 @@
 uniform float time;
+uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
+
+varying vec2 vUv;
 
 void main() {
-  gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0);
+  vUv = uv;
+  vec4 defaultState = modelMatrix * vec4(position, 1.0);
+  vec4 fullScreenState = vec4(position, 1.0);
+
+  fullScreenState.x *= uResolution.x / uQuadSize.x;
+  fullScreenState.y *= uResolution.y / uQuadSize.y;
+
+  vec4 finaleState = mix(defaultState, fullScreenState, uProgress);
+
+  gl_Position = projectionMatrix * viewMatrix * finaleState;
 }
