9f437e2754da24ab2e934f3c4c3b7e7fd9dd367f
Kiril Tucker <kiril.tucker@gmail.com>
2022-08-25 10:02:03 +0200

    Added ASScroll, Corrected Meshes Positioning, Commented goToSection until Fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
 import * as THREE from 'three'
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+import ASScroll from '@ashthornton/asscroll'
 import fragment from './shaders/fragment.glsl'
 import vertex from './shaders/vertex.glsl'
 import testTexture from '../img/test.jpg'
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollToPlugin)
 
 export default class Sketch {
   constructor(options) {
@@ -29,13 +33,172 @@ export default class Sketch {
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
     this.time = 0
+    this.sections = gsap.utils.toArray('.section');
+    this.images = gsap.utils.toArray('.js-image')
+    this.links = gsap.utils.toArray('a')
 
-
+    this.setSmoothScroll()
+    this.setScrollTrigger()
+    this.setMouseHover()
+    // this.setCounter()
     this.setupSettings()
     this.resize()
     this.addObjects()
     this.render()
     this.setupResize()
+  }
+
+  setupASScroll() {
+    // https://github.com/ashthornton/asscroll
+    const asscroll = new ASScroll({
+      // ease: 0.5,
+      disableRaf: true
+    })
+
+    gsap.ticker.add(asscroll.update)
+
+    ScrollTrigger.defaults({
+      scroller: asscroll.containerElement })
+
+
+    ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+      scrollTop(value) {
+        if (arguments.length) {
+          asscroll.currentPos = value
+          return
+        }
+        return asscroll.currentPos
+      },
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        }
+      },
+      fixedMarkers: true })
+
+
+    asscroll.on("update", ScrollTrigger.update)
+    ScrollTrigger.addEventListener("refresh", asscroll.resize)
+
+    requestAnimationFrame(() => {
+      asscroll.enable({
+        newScrollElements: document.querySelectorAll(".gsap-marker-start, .gsap-marker-end, [asscroll]")
+      })
+    })
+    return asscroll;
+  }
+
+  setSmoothScroll() {
+    this.asscroll = this.setupASScroll()
+  }
+
+  setScrollTrigger() {
+
+    // Change background color on scroll
+
+    this.sections.map((elem) => {
+
+      var bgColor = elem.getAttribute('data-color');
+
+      let trigger = ScrollTrigger.create({
+        trigger: elem,
+        start: 'top 5%',
+        end: 'bottom 5%',
+        markers: false,
+        onToggle() {
+          gsap.to('body', {
+            backgroundColor: bgColor,
+            duration: '1.2'
+          })
+        }
+      });
+
+      return () => {
+        bgColor = elem.getAttribute('data-color')
+        if (trigger.isActive) {
+          gsap.killTweensOf('body');
+          gsap.set('body', {
+            backgroundColor: bgColor
+          })
+        }
+      }
+    });
+
+    // // Immediate snap
+
+    // function goToSection(i) {
+    //   gsap.to(window, {
+    //     scrollTo: { y: i * innerHeight, autoKill: false, ease: 'Power3.easeInOut' },
+    //     duration: 0.8
+    //   })
+    //   document.querySelector('.currentNum').innerHTML = i + 1
+    // }
+
+    // this.sections.forEach((section, i) => {
+    //   const mainAnim = gsap.timeline({ paused: true })
+
+    //   ScrollTrigger.create({
+    //     trigger: section,
+    //     onEnter: () => goToSection(i),
+    //   })
+
+    //   ScrollTrigger.create({
+    //     trigger: section,
+    //     start: 'bottom bottom',
+    //     onEnterBack: () => goToSection(i)
+    //   })
+    // })
+  }
+
+  setMouseHover() {
+    // const animateDetails = (opacity, delay) => {
+    //   gsap.to('.details', {
+    //     opacity: opacity,
+    //     duration: 0.3,
+    //     delay: delay
+    //   })
+    // }
+
+    const animateTitle = (opacity) => {
+      gsap.to('.title', {
+        opacity: opacity,
+        duration: 0.3,
+      })
+    }
+
+    this.images.forEach((image, i) => {
+      image.addEventListener('mouseenter', () => {
+        gsap.to(image, {
+          scale: 1.1,
+          duration: 0.5,
+          overwrite: true,
+          onToggle: () => {
+            // animateDetails(0, 0),
+            animateTitle(0)
+          }
+        })
+      })
+
+      image.addEventListener('mouseleave', () => {
+        gsap.to(image, {
+          scale: 1,
+          duration: 0.5,
+          overwrite: true,
+          onToggle: () => {
+            // animateDetails(1, 0.2),
+            animateTitle(1)
+          }
+        })
+      })
+    })
+  }
+
+  setCounter() {
+    let totalNum = this.sections.length
+    document.querySelector('.totalNum').innerHTML = totalNum
   }
 
   setupSettings() {
@@ -100,7 +263,6 @@ export default class Sketch {
     this.mesh.scale.set(300, 300, 1)
     // this.scene.add( this.mesh )
 
-    this.images = [...document.querySelectorAll('.js-image')]
     this.materials = []
     const loader = new THREE.TextureLoader()
 
@@ -129,11 +291,19 @@ export default class Sketch {
     })
   }
 
+  setPosition() {
+    console.log(this.asscroll.currentPos);
+    this.imageStore.forEach(o =>{
+      o.mesh.position.x = o.left - this.width/2 + o.width/2
+      o.mesh.position.y = this.asscroll.currentPos + -o.top + this.height/2 - o.height/2
+    })
+  }
+
   render() {
     this.time += 0.05
     this.material.uniforms.uTime.value = this.time
     // this.material.uniforms.uProgress.value = this.settings.progress
-
+    this.setPosition()
     this.tl.progress(this.settings.progress)
 
     // this.mesh.rotation.x += 0.01
@@ -147,131 +317,4 @@ export default class Sketch {
 
 new Sketch({
   domElement: document.getElementById('container')
-})
-
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(ScrollToPlugin)
-
-let sections = gsap.utils.toArray(".section");
-let images = gsap.utils.toArray('.js-image')
-let links = gsap.utils.toArray('a')
-
-/**
- * Counter
- */
-
-let totalNum = sections.length
-document.querySelector('.totalNum').innerHTML = totalNum
-
-/**
- * GSAP
- */
-
-// Change background color on scroll
-
-gsap.utils.toArray(".section").map((elem) => {
-
-  var bgColor = elem.getAttribute('data-color');
-
-  let trigger = ScrollTrigger.create({
-    trigger: elem,
-    start: 'top 5%',
-    end: 'bottom 5%',
-    markers: false,
-    onToggle() {
-      gsap.to('body', {
-        backgroundColor: bgColor,
-        duration: '1.2'
-      })
-
-      // gsap.to('.title', {
-      //   color: bgColor,
-      //   duration: '1.2'
-      // })
-    }
-  });
-
-  return () => {
-    bgColor = elem.getAttribute('data-color')
-    if (trigger.isActive) {
-      gsap.killTweensOf('body');
-      gsap.set('body', {
-        backgroundColor: bgColor
-      })
-    }
-  }
-});
-
-// Immediate snap
-
-function goToSection(i) {
-  gsap.to(window, {
-    scrollTo: { y: i * innerHeight, autoKill: false, ease: "Power3.easeInOut" },
-    duration: 0.8
-  })
-  document.querySelector('.currentNum').innerHTML = i + 1
-}
-
-ScrollTrigger.defaults({
-  markers: false
-})
-
-sections.forEach((section, i) => {
-  const mainAnim = gsap.timeline({ paused: true })
-
-  ScrollTrigger.create({
-    trigger: section,
-    onEnter: () => goToSection(i),
-  })
-
-  ScrollTrigger.create({
-    trigger: section,
-    start: "bottom bottom",
-    onEnterBack: () => goToSection(i)
-  })
-})
-
-/**
- * Card Mouse Hover
- */
-
-// const animateDetails = (opacity, delay) => {
-//   gsap.to('.details', {
-//     opacity: opacity,
-//     duration: 0.3,
-//     delay: delay
-//   })
-// }
-
-const animateTitle = (opacity) => {
-  gsap.to('.title', {
-    opacity: opacity,
-    duration: 0.3,
-  })
-}
-
-images.forEach((image, i) => {
-  image.addEventListener("mouseenter", () => {
-    gsap.to(image, {
-      scale: 1.1,
-      duration: 0.5,
-      overwrite: true,
-      onToggle: () => {
-        // animateDetails(0, 0),
-        animateTitle(0)
-      }
-    })
-  })
-
-  image.addEventListener("mouseleave", () => {
-    gsap.to(images, {
-      scale: 1,
-      duration: 0.5,
-      overwrite: true,
-      onToggle: () => {
-        // animateDetails(1, 0.2),
-        animateTitle(1)
-      }
-    })
-  })
 })
